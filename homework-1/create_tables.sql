da588122044c68abfd129077c97dc9ddc2babe08
Nadya Guerra <nadyammex@gmail.com>
2023-06-15 18:07:49 -0700

    home 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,34 @@
 -- SQL-команды для создания таблиц
+
+
+CREATE TABLE customers_data
+(
+    customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) not null,
+	contact_name varchar(100) not null
+);
+
+SELECT * FROM customers_data
+
+CREATE TABLE employees_data
+(
+    employee_id int PRIMARY KEY,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	title  varchar(100),
+	birth_date date,
+	notes text
+);
+
+SELECT * FROM employees_data
+
+
+
+CREATE TABLE orders_data
+(
+    orders_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers_data(customer_id) not null,
+	employee_id int REFERENCES employees_data(employee_id) not null,
+	order_date date,
+	ship_city varchar(100) not null
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,68 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+import os
+
+
+
+BASE_PATH = os.path.abspath("north_data")
+EMPLOYEES_PATH = os.path.join(BASE_PATH, "employees_data.csv")
+CUSTOMERS_PATH = os.path.join(BASE_PATH, "customers_data.csv")
+ORDERS_PATH = os.path.join(BASE_PATH, "orders_data.csv")
+
+employees_list = []
+with open(EMPLOYEES_PATH, "r", encoding='utf-8') as csv_file:
+    reader = csv.DictReader(csv_file)
+    columns = ["employee_id", "first_name", "last_name", "title", "birth_date", "notes"]
+    if reader.fieldnames == columns:
+        for employee in reader:
+            employees_list.append((employee["employee_id"], employee["first_name"], employee["last_name"],
+                                   employee["title"], employee["birth_date"], employee["notes"]))
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="sadyuga2015")
+with conn:
+    with conn.cursor() as cur:
+        cur.executemany("INSERT INTO employees_data VALUES (%s, %s, %s, %s, %s, %s)",
+                        employees_list)
+        cur.execute("SELECT * FROM employees_data")
+        conn.commit()
+
+
+conn.close()
+
+customers_list = []
+with open(CUSTOMERS_PATH, "r", encoding="utf-8") as csv_file:
+    reader = csv.DictReader(csv_file)
+    columns = ["customer_id", "company_name", "contact_name"]
+    if reader.fieldnames == columns:
+        for customer in reader:
+            customers_list.append((customer["customer_id"], customer["company_name"], customer["contact_name"]))
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="sadyuga2015")
+with conn:
+    with conn.cursor() as cur:
+        cur.executemany("INSERT INTO customers_data VALUES (%s, %s, %s)",
+                        customers_list)
+
+        conn.commit()
+
+
+conn.close()
+
+orders_list = []
+with open(ORDERS_PATH, "r", encoding="utf-8") as csv_file:
+    reader = csv.DictReader(csv_file)
+    columns = ["order_id","customer_id","employee_id","order_date","ship_city"]
+    if reader.fieldnames == columns:
+        for order in reader:
+            orders_list.append((order["order_id"], order["customer_id"],order["employee_id"],order["order_date"],order["ship_city"]))
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="sadyuga2015")
+with conn:
+    with conn.cursor() as cur:
+        cur.executemany("INSERT INTO orders_data VALUES (%s, %s, %s,%s, %s)",
+                        orders_list)
+
+        conn.commit()
+
+conn.close()
